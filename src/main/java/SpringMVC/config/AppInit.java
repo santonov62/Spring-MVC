5e9f17478638007227678856e20a0a79fa2a13db
Sergey Antonov <flex62ryz@ya.ru>
2018-03-30 01:03:05 +0300

    init commit

---
+++ src/main/java/SpringMVC/config/AppInit.java
@@ -0,0 +1,42 @@
+package SpringMVC.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext)
+            throws ServletException {
+        super.onStartup(servletContext);
+        ServletRegistration.Dynamic servlet = servletContext
+                .addServlet("h2-console", new WebServlet());
+        servlet.setLoadOnStartup(2);
+        servlet.addMapping("/console/*");
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                WebConfiguration.class
+        };
+    }
+
+    // Тут добавляем конфигурацию, в которой инициализируем ViewResolver
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+        return new Class<?>[]{
+                WebConfiguration.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
